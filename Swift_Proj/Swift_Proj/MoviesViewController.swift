69fea6843a9246cbf07b7e2848f4fd5216b74c63
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-05 19:16:12 +0200

    add movies in a collection view

---
+++ Swift_Proj/Swift_Proj/MoviesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoviesViewController.swift
+//  Swift_Proj
+//
+//  Created by Eslam Ali  on 05/02/2022.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
