8380ad38d1ccc890038bf349a663fc81addef85f
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-02-08 18:43:44 +0200

    add segmented control pages

---
+++ Swift_Proj/Swift_Proj/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  Swift_Proj
+//
+//  Created by Eslam Ali  on 08/02/2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
